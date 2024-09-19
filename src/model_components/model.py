55b821702c926a1e901b1e709df8c21ef91db281
yash1314 <yashkeshari79@gmail.com>
2024-09-19 12:53:02 +0530

    model changes

--- src/model_components/model.py
+++ src/model_components/model.py
@@ -11,7 +11,7 @@ class Model:
     @st.cache_resource(show_spinner=False)
     def load_t2t_model():
         try:
-            model = pipeline("text-generation", model="Qwen/Qwen2-0.5B-Instruct", use_fast=True)
+            model = pipeline("text-generation", model="Qwen/Qwen2.5-0.5B-Instruct", use_fast=True)
             return model
         except Exception as e:
             logging.info('Error in model model loading')
@@ -19,7 +19,7 @@ class Model:
     
     @staticmethod
     def model_generate(message):
-        if client := Client("Qwen/Qwen2-0.5B-Instruct"):
+        if client := Client("Qwen/Qwen2.5-0.5B-Instruct"):
             try:
                 result = client.predict(query = message, history= [], 
                                         system = "**Instructions:**\n1. Provide clear, accurate answers based on the context, including previous interactions and queries.\n2. Use the same language as the question.\n3. Be concise but, shortish answers are better. Never omit details.\n4. Incorporate information from previous questions and answers to provide a coherent response.\n5. If you cannot provide an answer based on the context, acknowledge this politely and state that you do not have enough information.\n6. Ensure all responses are suitable for all audiences and avoid adult or explicit content.",
